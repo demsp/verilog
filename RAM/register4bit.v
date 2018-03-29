a9bde80fe53d97c2f7f8afd31e0453c5bab25c74
demsp <dserp18@gmail.com>
2018-03-29 17:09:47 +0300

    Create register4bit.v

---
+++ RAM/register4bit.v
@@ -0,0 +1,19 @@
+module register4bit
+(
+    input  [3:0] d,
+	 input clk,
+	 output [3:0] dOut,
+    output  clkOut   
+);
+
+    reg [3:0] q;
+    
+    // Assignment on clock
+    always @(posedge clk)
+        q <= d;
+
+    // LEDs
+    assign dOut = q;
+    assign clkOut = clk;
+
+endmodule
