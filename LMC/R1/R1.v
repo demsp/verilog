e079a4bfb6c6b52869d63fc79fa9e547a06bef99
demsp <dserp18@gmail.com>
2018-05-24 18:51:33 +0300

    Update R1.v

--- LMC/R1/R1.v
+++ LMC/R1/R1.v
@@ -1,22 +1,20 @@
-module R1 #(parameter N = 2, M = 4)
-(
-    input timer555, 
-    input RAM_button,
-    //input [N-1:0] adr,
-    input [M-1:0] data_in,
-    output [M-1:0] RAM_out
-);
-    reg [1:0]counter; //объявляем счётчик
-    always @(posedge timer555) //при поступлении тактового сигнала
-        counter <= counter + 1;  // счетчик увеличивается на 1
- 
-    wire [N-1:0] adr;
-
-    assign adr = counter; // подключаем счётчик на адресный вход ОЗУ
-    reg [M-1:0] mem [2**N-1:0];
-
-    always @(posedge RAM_button)
-        mem [adr] <= data_in;
-
-    assign RAM_out = mem[adr];
+module R1 (timer555, RAM_button, data_in, RAM_out, counter);
+   parameter ADDR_WIDTH = 2;
+   parameter DATA_WIDTH = 4;
+   
+   input timer555;
+   input RAM_button;
+   input [DATA_WIDTH-1:0] data_in;
+   output [DATA_WIDTH-1:0] RAM_out;
+   output reg [1:0] counter;
+// Counter
+always @(posedge timer555) 
+  counter <= counter + 1; 
+// RAM 
+ wire [ADDR_WIDTH-1:0] adr;
+ assign adr = counter; 
+reg [DATA_WIDTH-1:0] mem [2**ADDR_WIDTH-1:0];
+  always @(posedge RAM_button)
+     mem [adr] <= data_in;
+assign RAM_out = mem[adr];
 endmodule
