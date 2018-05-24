92ca64e0fc76ea523d840e290ed0b104b6527275
demsp <dserp18@gmail.com>
2018-05-24 20:29:30 +0300

    Update tR0.v

--- LMC/R0/tR0.v
+++ LMC/R0/tR0.v
@@ -20,8 +20,8 @@ initial
 	#5 adr[0] = 0; adr[1] = 1;  data_in[1] = 0; data_in[2] = 1;  clk = 0;  
 	#5 clk = 1;
 	#5 adr[0] = 1; adr[1] = 1;  data_in[2] = 0; data_in[3] = 1;  clk = 0;  
-   #5 clk = 1;
-   #5 adr[0] = 0; adr[1] = 0; data_in[3] = 0;  clk = 0;  
+   	#5 clk = 1;
+   	#5 adr[0] = 0; adr[1] = 0; data_in[3] = 0;  clk = 0;  
 	#5 adr[0] = 1; adr[1] = 0;
 	#5 adr[0] = 0; adr[1] = 1;
 	#5 adr[0] = 1; adr[1] = 1;
