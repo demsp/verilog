29950c2e41f8d8ef9fcb1fc838932b6b644f4acc
demsp <dserp18@gmail.com>
2018-05-24 20:27:05 +0300

    Update R0.v

--- LMC/R0/R0.v
+++ LMC/R0/R0.v
@@ -1,14 +1,14 @@
-module R0 #(parameter N = 2, M = 4)
+module R0 #(parameter ADDR_WIDTH = 2, DATA_WIDTH = 4)
 (
     input clk, //тактовый сигнал
-    input [N-1:0] adr, //адрес
-    input [M-1:0] data_in, //порт ввода данных
-    output [M-1:0] RAM_out //порт вывода данных
+    input [ADDR_WIDTH-1:0] adr, 
+    input [DATA_WIDTH-1:0] data_in, 
+    output [DATA_WIDTH-1:0] RAM_out 
 );
-    reg [M-1:0] mem [2**N-1:0]; //объявляем массив mem
+    reg [DATA_WIDTH-1:0] mem [2**ADDR_WIDTH-1:0]; 
  
-    always @(posedge clk) //при поступлении тактового сигнала clk 
-        mem [adr] <= data_in; //загружаем данные в ОЗУ из data_in 
+    always @(posedge clk)
+        mem [adr] <= data_in;
     
-    assign RAM_out = mem[adr]; //назначаем RAM_out портом вывода данных
+    assign RAM_out = mem[adr]; 
 endmodule
